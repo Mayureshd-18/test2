44124be613c7e64ef3f36c201930c051d8e0dab1
Mayuresh Dharwadkar <98738585+Mayureshd-18@users.noreply.github.com>
2022-10-12 17:13:50 +0000

    Sync LeetCode submission - Longest Common Prefix (python3)

---
+++ my-folder/problems/longest_common_prefix/solution.py
@@ -0,0 +1,7 @@
+from itertools import takewhile
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ''.join(c[0] for c in takewhile(lambda x:all(x[0] == y for y in x), zip(*strs)))
+        
+        return str(res)
+        
