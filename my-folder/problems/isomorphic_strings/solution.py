ce5a57536f253b4bc8d725a42d804ac3f87fe064
Mayuresh Dharwadkar <98738585+Mayureshd-18@users.noreply.github.com>
2022-11-05 17:19:52 +0000

    Sync LeetCode submission - Isomorphic Strings (python3)

---
+++ my-folder/problems/isomorphic_strings/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if s == t == "":
+            return True
+        
+        compare = {}
+        for i in range(len(s)):
+            if s[i] not in compare: 
+                if t[i] in compare.values():
+                    return False
+                compare[s[i]] = t[i]
+            else:
+                if compare[s[i]] != t[i]:
+                    return False
+       
+        return True
