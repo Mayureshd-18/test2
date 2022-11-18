8a7535265dd689b3f28d882edba42c5e8553bc22
Mayuresh Dharwadkar <98738585+Mayureshd-18@users.noreply.github.com>
2022-11-18 13:18:55 +0000

    Sync LeetCode submission - Ugly Number (python3)

--- my-folder/problems/ugly_number/solution.py
+++ my-folder/problems/ugly_number/solution.py
@@ -1,24 +1,19 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        pfact = []
-        ugly = [2,3,5]
-        if n<=0:
-            return False
-        while n % 2 == 0:
-                pfact.append(2)
-                n = n / 2
 
-        for i in range(3,int(math.sqrt(n))+1,2):
-    
-            while n % i== 0:
-                pfact.append(i)
-                n = n / i
+        while n>0:
+            if n == 1:
+                return True
+            if n % 2 == 0:
+                n //= 2
+                continue
+            elif n % 3 == 0:
+                n //= 3
+                continue
+            elif n % 5 == 0:
+                n //= 5
+                continue
+            break
 
-        if n > 2:
-            pfact.append(n)
-        print(pfact)
-        for i in pfact:
-            if i not in ugly:
-                return False
-        return True
-    
+
+        return False
