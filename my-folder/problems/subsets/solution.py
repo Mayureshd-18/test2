c7b04e46cc1f7be847e35a54e5ffc9ba8be7881a
Mayuresh Dharwadkar <98738585+Mayureshd-18@users.noreply.github.com>
2022-12-02 12:41:13 +0000

    Sync LeetCode submission - Subsets (python3)

---
+++ my-folder/problems/subsets/solution.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        comb = []
+        for i in range(len(nums)+1):
+            comb += [list(j) for j in combinations(nums, i)]
+        return comb
